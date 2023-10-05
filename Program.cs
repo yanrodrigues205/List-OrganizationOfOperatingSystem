a2661e4390ec9af6a8f7b74c290d2ff59a3243d3
yanrodrigues205 <yan.pablo205@gmail.com>
2023-10-05 20:14:36 -0300

    adicionando reparticoes

--- Program.cs
+++ Program.cs
@@ -4,20 +4,26 @@ class Program
 {
     static void Main()
     {
-        Lista lista = new Lista();
+        Lista lista_processos = new Lista();
+        Lista lista_vazia = new Lista();
         
 
-        lista.Adicionar("Valor 1");
-        lista.Adicionar("Valor 2");
-        lista.Adicionar("Valor 3");
-        lista.Adicionar("Valor 4");
-        lista.Remover("Valor 3");
-        lista.Remover("Valor 2");
+        Console.WriteLine("Yan Pablo Rodrigues - 1 ANO AMS-ADS\n Partições de Memória");
+        Console.WriteLine("\nLista de Processos:");
+        lista_processos.Adicionar("[A|5|3]");
+        lista_processos.Adicionar("[B|8|6]");
+        lista_processos.Adicionar("[C|14|2]");
+        lista_processos.Adicionar("[D|16|3]");
+        lista_processos.Adicionar("[?|19|1]");
+        lista_processos.Adicionar("[E|26|3]");
+        lista_processos.Adicionar("[?|29|3]");
+        lista_processos.Mostrar();
+        Console.WriteLine("\nLista de Espaços Vazios:");
+        lista_vazia.Adicionar("[-|0|5]");
+        lista_vazia.Adicionar("[-|20|6]");
+        lista_vazia.Mostrar();
 
 
-        lista.Pesquisar("Vaalor 1");
-        lista.Editar("Valor 1", "Valor 123");
-        lista.Mostrar();
 
     }
 }
