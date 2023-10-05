f267579b98e5cd588bd045e9a649a238e1df64b9
unknown <yan.pablo205@gmail.com>
2023-10-05 15:39:54 -0300

    first commit

---
+++ List/No.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lista_EngenhariaDSoftware.List
+{
+    public class No
+    {
+        
+    }
+}
